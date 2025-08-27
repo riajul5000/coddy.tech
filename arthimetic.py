8bc140261d03335926cf26c1b927f60aa27a1173
Riajul Islam <riyazulislam500@gmail.com>
2025-08-27 21:42:24 +0200

    arthimetic and recap math added

---
+++ arthimetic.py
@@ -0,0 +1,34 @@
+a = 50 
+a -= 10
+print(a)
+
+# CHALLENGE!!
+# You are given a code with initialization of count. (Don't delete this line!)
+
+# Your task is to add the following operations, in this order:
+
+# Add 4 to count
+# Multiply count by 2
+# Subtract 1 from count
+# Use the arithmetic shortcuts to do so 
+
+count = 4 
+count *= 2 
+count -= 1
+print(count)
+
+#  challenge!!
+# You are given a code with initialization of score. (Don't delete this line!)
+
+# Your task is to modify the score variable using arithmetic shortcuts, in this order:
+
+# Divide score by 2
+# Add 10 to score
+# Multiply score by 3
+# Remember to use the arithmetic shortcuts for each operation! result= 180.0
+
+score = 100
+score /= 2 
+score += 10
+score *= 3
+print(score)
---
+++ recap_map.py
@@ -0,0 +1,6 @@
+# You are given a code, initialize the variables a and b so that c will hold 24.
+
+a = 6
+b = 4
+c = (a * b)
+print(c)
