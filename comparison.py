89bcd608b27410e4998f741808548af17273d071
Riajul Islam <riyazulislam500@gmail.com>
2025-08-27 22:06:18 +0200

    comparison practice added

---
+++ comparison.py
@@ -0,0 +1,35 @@
+# Equal
+score = 50
+score1 = 60
+score3 = score==score1
+print(score3)
+
+#  not Equal 
+var = 50
+var1 = 40
+var3 = var!=var1
+print(var3)
+
+#  greater than
+a = 3
+b = 50
+c = b > a
+print(c)
+
+# less than
+x = 50
+y = 60
+z = y < x
+print(z)
+
+# Greater than or equal
+f = 59
+f1 = 69
+f2 = f >= f1
+print(f2)
+
+# less than or equal
+g = 59
+g1 = 60
+g2 = g <= g1
+print(g2)
