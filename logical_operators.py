378ae02100659e5e4a5c1fc20f314580df8dc443
Riajul Islam <riyazulislam500@gmail.com>
2025-08-30 17:39:48 +0200

    logical some code added

--- logical_operators.py
+++ logical_operators.py
@@ -1,8 +1,58 @@
 age = 20
 have_license = True
 result = age >= 18 and have_license
-print("he have license:",  result )
+# print("he have license:",  result )
 
 numbers = 50
 result1 = not (numbers > 10 and numbers< 4)
-print(result1)
+# print(result1)
+
+#  python logical operators table from (and) 
+x = True
+z = False
+y = x and z
+# print(y)
+
+
+x1 = True
+z1 = True
+y1 = x1 and z1
+# print(y1)
+
+x2 = False
+z2 = True
+y2 = x2 and z2
+# print(y2)
+
+x3 = True
+z3 = False
+y3 = x3 and z3
+# print(y3)
+
+# logical operators table form or
+
+t = True
+r = False
+u = t or r 
+# print(u)
+
+t1 = False
+r1 = False
+u1 = t1 or r1 
+# print(u1)
+
+t2 = False
+r2 = True
+u2 = t2 or r2
+# print(u2)
+
+t3 = True
+r3 = True
+u3 = t3 or r3 
+# print(u3)
+
+# logical operators not
+
+q = False
+e =  not q 
+# print(e)
