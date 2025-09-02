93a2b31e74ee98a01c900b1bcb1988ffa8cfbcd3
Riajul Islam <riyazulislam500@gmail.com>
2025-09-02 21:34:01 +0200

    data types learning

--- casting.py
+++ casting.py
@@ -1,5 +1,5 @@
 # x = float(input(2.3))
 # print(x)
 
-y = int(input('I will be programmer'))
-print(f"Studying hard then{y}")
+y = str('I will be programmer')
+print(f"Studying hard then {y}")
---
+++ data_types.py
@@ -0,0 +1,9 @@
+# x = str("They called data types")
+# # print(x)
+
+# y = input(50)
+# # print(y)
+
+# z = float(59.0)
+# print(float(z))
+
