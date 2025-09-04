85841df32bb1ea4d19b1ed7ed4828f2b1a40f5e3
Riajul Islam <riyazulislam500@gmail.com>
2025-09-04 21:15:13 +0200

    Data types practice

--- data_types.py
+++ data_types.py
@@ -7,3 +7,26 @@
 # z = float(59.0)
 # print(float(z))
 
+fruits_list = list(("apple" , "banana" , "mango"))
+# print(fruits_list)
+
+car_lists = tuple(("bmw" , "mercedes" , "fiat"))
+# print(car_lists)
+
+u = range(15)
+# print(u)
+
+id_name = {"name" : "Elon" , "age" : 44}
+# print(id_name)
+
+h = u"hello"
+# print(h)
+
+w = bytearray(5)
+# print(w)
+
+e = memoryview(bytes(5))
+print(e)
+
+reason = None
+print(None)
