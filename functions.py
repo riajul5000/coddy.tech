78d223ab098a67281987090a96f78c5930ae6bb7
Riajul Islam <riyazulislam500@gmail.com>
2025-09-15 19:49:12 +0200

    just some practice function

--- functions.py
+++ functions.py
@@ -16,6 +16,18 @@ num1 = 3
 num2 = 5
 def my_count(num1, num2):
     print(num1 / num2)
-my_count(num1, num2)
+# my_count(num1, num2)
 
 
+n = 5
+def square_number(n):
+    return n * n
+result = square_number(5)
+# print(result)
+
+
+def cube_number(n):
+    return n ** 3
+input_num = 89
+result = cube_number(input_num)
+print(result)
