01f5da24796abb7ebe4df830ff186085a9b0e44f
Riajul Islam <riyazulislam500@gmail.com>
2025-09-15 19:15:47 +0200

    function practice

--- for_seccond.py
+++ for_seccond.py
@@ -4,3 +4,6 @@ for i in range(1, n + 1):
     for j in range(1, n + 1):
         if i * j == n:
             print(i, j)
+
+for x in range(15, 34):
+    print(x)
--- functions.py
+++ functions.py
@@ -4,4 +4,18 @@ def my_function():
 
 def my_city():
     print("Rome")
-my_city()
+# my_city()
+
+def my_fall():
+    print("Hello World")
+n = 5
+# for i in range(n):
+#  my_fall()
+
+num1 = 3
+num2 = 5
+def my_count(num1, num2):
+    print(num1 / num2)
+my_count(num1, num2)
+
+
