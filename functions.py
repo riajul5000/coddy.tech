be37bf1a89ce839928ca1215043c0964bfba8031
Riajul Islam <riyazulislam500@gmail.com>
2025-09-18 21:08:01 +0200

    Still running function

--- functions.py
+++ functions.py
@@ -30,4 +30,39 @@ def cube_number(n):
     return n ** 3
 input_num = 89
 result = cube_number(input_num)
+# print(result)
+
+
+def sigma(n):
+    sum = 0
+    for i in range(1, n + 1):
+     sum += i
+    return sum 
+result = sigma(5)
+# print(result)
+
+
+def count_numbers(x):
+    total = 0
+    for i in range(10, x):
+     total += i
+    return total
+result1 = count_numbers(60)
+# print(result1)
+
+def have_car(y):
+   total = 1
+   for i in range(20, y):
+      total *= i
+   return total
+result = have_car(60)
+# print(result)
+
+
+def tour_area(z):
+   total = 7
+   for i in range(10, z):
+      total /= i 
+   return total
+result = tour_area(70)
 print(result)
--- numbers.py
+++ numbers.py
@@ -1,19 +1,35 @@
-x = 5
-y = 10
-z = (x*y)
+# x = 5
+# y = 10
+# z = (x*y)
 
-# print(z)
-
-
-
-a = 50
-b = 70
-# print(a*b)
+# # print(z)
 
 
-c = 689
-d = 700
-e = (d - c)
-f = (e + 20)
 
-print(f)
+# a = 50
+# b = 70
+# # print(a*b)
+
+
+# c = 689
+# d = 700
+# e = (d - c)
+# f = (e + 20)
+
+# print(f)
+
+# Read input string
+
+numbers_str = input( 1, 2 , 3)
+
+# Split into list of strings
+numbers_list = numbers_str.split(", ")
+
+# Convert to integers
+numbers = [int(num) for num in numbers_list]
+
+# Calculate sum
+total = sum(numbers)
+
+# Print result
+print(total)
