db97d05e050a32adcd8d4a04e0d4f555bf341103
Riajul Islam <riyazulislam500@gmail.com>
2025-09-22 21:25:07 +0200

    Global variable recap

--- onemonth_recap/variable.py
+++ onemonth_recap/variable.py
@@ -15,7 +15,7 @@ x = y = z = "Apple"
 
 cars = ["BMW" , "MERCEDES", "AUDI"]
 x, y, z = cars
-print(x)
+# print(x)
 # print(y)
 # print(z)
 
@@ -23,10 +23,49 @@ print(x)
 x = "Python"
 y = "is"
 z = "awesome"
-print(x, y, z)
+# print(x, y, z)
 
 x = "Python "
 y = "is "
 z = "awesome"
-print(x + y + z)
+# print(x + y + z)
 
+# Variables that are created outside of a function (as in all of the examples in the previous pages) are known as global variables.
+
+# Global variables can be used by everyone, both inside of functions and outside.
+
+#GLOBAL VARIABLE
+
+x = "awesome"
+def city():
+    print("Oslo city is" + x)
+# city()
+
+x = "awesome"
+
+def some_city():
+    x = "Dangerous"
+    print("Milan is " + x)
+
+# some_city()
+
+# print("Rome is " + x)
+
+# Global keyword
+
+
+
+def my_city():
+    global x 
+    x = "Awesome"
+# my_city()
+# print("My home city is " + x)
+
+
+x = "Awesome"
+
+def your_house():
+    global x
+    # x = " Fantastic"
+your_house()
+print("Your house is " + x)
